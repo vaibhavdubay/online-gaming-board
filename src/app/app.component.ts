2ebbaaa43ff6de280f3bf775bda7e765aa5678a2
Vaibhav Dubay <sirvaibhavdubay@gmail.com>
2023-08-15 18:42:31 +0530

    updated ngrx store and schematics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'online-gaming-board';
+  public title = 'online-gaming-board';
 }
