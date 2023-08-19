3df465ce303799c52644181107d7a78254a07f7c
Vaibhav Dubay <sirvaibhavdubay@gmail.com>
2023-08-20 03:07:30 +0530

    updated firebase config

--- src/app/modules/authenticate/authenticate.module.ts
+++ src/app/modules/authenticate/authenticate.module.ts
@@ -11,9 +11,7 @@ import { AuthenticateService } from './service/authenticate.service';
 import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
+  declarations: [LoginComponent],
   imports: [
     CommonModule,
     AuthenticateRoutingModule,
